80e1e7336357c408549d644ac28fccf560d6c3ab
Omar Bracamonte <omar.bracamonte@gmail.com>
2017-10-23 15:16:39 -0700

    rombo

--- src/ExampleProgram.java
+++ src/ExampleProgram.java
@@ -1,16 +1,47 @@
 //A Very Simple Example
+import java.util.Scanner;
+
 class ExampleProgram {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+//        Scanner obs = new Scanner(System.in);
+        int i, j, izq, der;
+        int lvl = 6;
+//        System.out.println("De cuantos niveles quieres la piramide?:");
+//        int lvl = obs.nextInt();
+        izq = lvl;
+        der = lvl;
+        for (i = 0; i < lvl; i++) {
+            for (j = 1; j < lvl * 2; j++) {
+                if (j < izq || j > der) {
+                    System.out.print(". ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            izq--;
+            der++;
+            System.out.println();
 
-        System.out.print("I'm a Simple Program\n\n\n\n");
+// parte de abajo
 
-        System.out.println("I'm a Simple Program");
+        izq = izq +1;
+        der = der -1;
+            for (i = lvl; i > 0; i--) {
+                for (j=(lvl*2)-1; j >0; j--) {
+                    if (j < izq || j > der) {
+                        System.out.print("  ");
+                    } else {
+                        System.out.print("* ");
+                    }
 
-        System.out.println("I'm a Simple Program");
+                }
+                izq++;
+                der--;
+                System.out.println();
+            }
+        }
 
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
     }
 }
+
