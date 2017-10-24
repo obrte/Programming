eac4c24139516c5352036a295da660c0c73f08d0
obrte <omar.bracamonte@gmail.com>
2017-10-23 21:40:07 -0700

    rombo mac

--- src/ExampleProgram.java
+++ src/ExampleProgram.java
@@ -5,15 +5,20 @@ class ExampleProgram {
 
     public static void main(String[] args) {
 //        Scanner obs = new Scanner(System.in);
-        int i, j, izq, der;
-        int lvl = 6;
-//        System.out.println("De cuantos niveles quieres la piramide?:");
+        int i, j, izq, der, lvl;
+//        System.out.println("De cuantos niveles quieres el rombo?: ");
 //        int lvl = obs.nextInt();
-        izq = lvl;
-        der = lvl;
-        for (i = 0; i < lvl; i++) {
-            for (j = 1; j < lvl * 2; j++) {
-                if (j < izq || j > der) {
+        lvl = 6;
+        if(lvl%2==0) {
+            izq = (lvl / 2) - 1;
+            der = (lvl / 2) - 1;
+        }else {
+            izq = lvl/2;
+            der = lvl/2;
+        }
+        for(i=0; i<lvl; i=i+2) {
+            for (j = 0; j < lvl; j++) {
+                if (j < izq || j> der) {
                     System.out.print(". ");
                 } else {
                     System.out.print("* ");
@@ -21,27 +26,41 @@ class ExampleProgram {
             }
             izq--;
             der++;
-            System.out.println();
+            System.out.println("");
+        }
 
-// parte de abajo
-
-        izq = izq +1;
-        der = der -1;
-            for (i = lvl; i > 0; i--) {
-                for (j=(lvl*2)-1; j >0; j--) {
-                    if (j < izq || j > der) {
-                        System.out.print("  ");
+        //par
+        if(lvl%2==0){
+            der= der-1;
+            izq= izq+1;
+            for(i=lvl;i>2;i=i-2){
+                for(j=lvl-2;j>=0;j--){
+                    if (j < izq || j> der) {
+                        System.out.print("- ");
                     } else {
                         System.out.print("* ");
                     }
-
                 }
                 izq++;
                 der--;
-                System.out.println();
+                System.out.println("");
+            }
+            // inpar
+        }else {
+            der = der - 2;
+            izq = izq + 2;
+            for (i = lvl / 2; i > 0; i = i - 1) {
+                for (j = lvl - 1; j >= 0; j--) {
+                    if (j < izq || j > der) {
+                        System.out.print("- ");
+                    } else {
+                        System.out.print("* ");
+                    }
+                }
+                izq++;
+                der--;
+                System.out.println("");
             }
         }
-
     }
 }
-
