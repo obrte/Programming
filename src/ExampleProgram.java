ecff8bdfa3707a9a3716cd67f91148eb045d904c
Omar Bracamonte <omar.bracamonte@gmail.com>
2017-10-25 13:25:37 -0700

    fix pares

--- src/ExampleProgram.java
+++ src/ExampleProgram.java
@@ -9,34 +9,53 @@ class ExampleProgram {
 //        System.out.println("De cuantos niveles quieres el rombo?: ");
 //        int lvl = obs.nextInt();
         lvl = 6;
+        //PARES
         if(lvl%2==0) {
             izq = (lvl / 2) - 1;
             der = (lvl / 2) - 1;
-        }else {
-            izq = lvl/2;
-            der = lvl/2;
-        }
-        for(i=0; i<lvl; i=i+2) {
-            for (j = 0; j < lvl; j++) {
-                if (j < izq || j> der) {
-                    System.out.print(". ");
-                } else {
-                    System.out.print("* ");
+            for(i=1; i<lvl; i=i+2) {
+                for (j = -1; j < lvl; j++) {
+                    if (j < izq || j> der) {
+                        if((i>lvl/2)&&(j<0 || j>lvl-2)) {
+                            System.out.print(". ");
+                        }else {
+                            System.out.print("+ ");
+                        }
+                    } else {
+                        System.out.print("* ");
+                    }
                 }
+                izq--;
+                der++;
+                System.out.println("");
             }
-            izq--;
-            der++;
-            System.out.println("");
+         //IMPAR
+        }else {
+            izq = lvl / 2;
+            der = lvl / 2;
+
+            for (i = 0; i < lvl; i = i + 2) {
+                for (j = 0; j < lvl; j++) {
+                    if (j < izq || j > der) {
+                        System.out.print("  ");
+                    } else {
+                        System.out.print("* ");
+                    }
+                }
+                izq--;
+                der++;
+                System.out.println("");
+            }
         }
 
         //par
         if(lvl%2==0){
             der= der-1;
             izq= izq+1;
-            for(i=lvl;i>2;i=i-2){
-                for(j=lvl-2;j>=0;j--){
+            for(i=lvl;i>1;i=i-2){
+                for(j=lvl-1;j>=0;j--){
                     if (j < izq || j> der) {
-                        System.out.print("- ");
+                        System.out.print("  ");
                     } else {
                         System.out.print("* ");
                     }
@@ -52,7 +71,7 @@ class ExampleProgram {
             for (i = lvl / 2; i > 0; i = i - 1) {
                 for (j = lvl - 1; j >= 0; j--) {
                     if (j < izq || j > der) {
-                        System.out.print("- ");
+                        System.out.print("  ");
                     } else {
                         System.out.print("* ");
                     }
